f5e19b1891c4c76e4bc5a66d64a9acdc7964d818
Christopher Davis <cdavis9999@gmail.com>
2012-11-14 21:42:14 -0600

    how to show admin notices

---
+++ admin-notice-example.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Admin Notice Example
+Plugin URI: http://wordpress.stackexchange.com/q/72637/6035
+Description: How to add admin notices.
+Author: Christopher Davis
+Author URI: http://christopherdavis.me
+License: GPL2
+
+    Copyright 2012 Christopher Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+add_action('admin_notices', 'wpse72637_show_names');
+/**
+ * An example of how to show an admin notice.
+ *
+ * @uses    get_plugins
+ * @return  void
+ */
+function wpse72637_show_names()
+{                  
+    $paths = array();
+
+    foreach(get_plugins() as $p_basename => $plugin)
+    {
+        $filename = is_plugin_active($p_basename) ?
+            "{$plugin['Name']}: Active" : "{$plugin['Name']}: Disabled";
+        $paths[] = $filename;   
+    }
+
+    echo '<div class="error">', implode(' --- ', $paths), '</div>';
+}
+
